e73ba26ce47dfade2b98bb9af813f7e53794fcd2
christopherembate <christopherembate@65gmail.com>
2025-11-09 05:32:04 -0800

    Update

--- CreateAccount.Designer.cs
+++ CreateAccount.Designer.cs
@@ -147,9 +147,9 @@
             this.SignInCreate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.SignInCreate.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SignInCreate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(44)))), ((int)(((byte)(42)))));
-            this.SignInCreate.Location = new System.Drawing.Point(210, 325);
+            this.SignInCreate.Location = new System.Drawing.Point(220, 332);
             this.SignInCreate.Name = "SignInCreate";
-            this.SignInCreate.Size = new System.Drawing.Size(181, 35);
+            this.SignInCreate.Size = new System.Drawing.Size(163, 35);
             this.SignInCreate.TabIndex = 9;
             this.SignInCreate.Text = "Sign In";
             this.SignInCreate.UseVisualStyleBackColor = false;
@@ -161,9 +161,9 @@
             this.SignUp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.SignUp.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SignUp.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(44)))), ((int)(((byte)(42)))));
-            this.SignUp.Location = new System.Drawing.Point(392, 325);
+            this.SignUp.Location = new System.Drawing.Point(397, 331);
             this.SignUp.Name = "SignUp";
-            this.SignUp.Size = new System.Drawing.Size(181, 35);
+            this.SignUp.Size = new System.Drawing.Size(161, 35);
             this.SignUp.TabIndex = 10;
             this.SignUp.Text = "Sign Up";
             this.SignUp.UseVisualStyleBackColor = false;
@@ -206,7 +206,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "CreateAccountPage";
             this.Text = "CreateAccount Page";
